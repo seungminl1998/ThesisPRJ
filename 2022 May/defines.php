63c9921772bcf3996bd3b962d7eaf8944c5d1193
seungminl1998 <101083759+seungminl1998@users.noreply.github.com>
2022-08-17 12:17:27 +0900

    Add files via upload

---
+++ 2022 May/defines.php
@@ -0,0 +1,17 @@
+<?php
+	session_start();
+
+	define( 'FACEBOOK_APP_ID', '3217833905153910' );
+	define( 'FACEBOOK_APP_SECRET', '●●●●●●●●' );
+	define( 'FACEBOOK_REDIRECT_URI', 'https://inseo.co.kr/inseo/index.php' );
+	define( 'ENDPOINT_BASE', 'https://graph.facebook.com/v13.0/' );
+
+	// accessToken
+	$accessToken = 'AAtumlbc53YBAHoJVo3uqVQiUIFAnXsgblNfTWTFHvdpejt7avZASJScr4x3OzONFyqrEBR2f3NI9dfAZCIpqzg39P4VFcAHUPHic49VXImPWi8EzfRzoMrF7KK8tTLHTMAuyNcCPqom9WO8AHxf64CsRlNWmkGK3CDhE8AJvyRNzaENyO
+	';
+
+	// page id
+	$pageId = 'MYID';
+
+	// instagram business account id
+	$instagramAccountId = 'thealexleee';
